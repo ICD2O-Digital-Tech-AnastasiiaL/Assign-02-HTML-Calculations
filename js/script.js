e7fe76a88fb899a000cfb1d0f50c1f13f1ef79e6
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-04-09 12:51:34 +0000

    Created my Assigment 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: April 2025
 // This file contains the JS functions for index.html
+"use strict";
+/**
+ * This function calculates the area of the triangle
+ */
+function calculateArea() {
+  // input
+  let sideA = parseInt(document.getElementById('sideA-of-the-decagon').value);
+
+  // process
+  let area = (5*sideA ** 2)/2 * Math.sqrt(5+2 * Math.sqrt(5))
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
+
+function calculatePerimeter() {
+  // input
+  let sideA = parseInt(document.getElementById('sideAsecond-of-the-decagon').value);
+
+  // process
+  let perimeter = 10 * sideA
+
+  // output
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm²'
+}
